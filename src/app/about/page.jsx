58ef4c4906fbb815c2375b5e18924b4a0c005241
mankinde23 <damilolamakinde493@gmail.com>
2024-01-24 05:03:05 +0100

    added css file to each page folder for styling

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from "./page.module.css";
 
 export default function About() {
   return (
-    <div>
+    <div className={styles.container}>
       About
     </div>
   )
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from "./page.module.css";
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className={styles.container}>
       Portfolio
     </div>
   )
