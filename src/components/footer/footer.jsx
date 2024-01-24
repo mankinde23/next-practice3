a2a67f334cb8700324281a2ef3890454938b5c9c
mankinde23 <makinded12@gmail.com>
2024-01-24 03:48:45 +0100

    understood the difference between this two component layout.jsx and page.jsx

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 5,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <div>
+      <Link href="/">Lamammia</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {" "}
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
